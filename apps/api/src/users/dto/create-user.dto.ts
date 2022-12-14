99cbc6d5a99105c889e0fdfb8cb2364edb024af0
joshunrau <contact@joshuaunrau.com>
2022-12-14 13:11:28 -0500

    check password strength

--- apps/api/src/users/dto/create-user.dto.ts
+++ apps/api/src/users/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 import { UserRole, UserType } from '@dnp/common/types';
-import { IsNotEmpty, IsIn, IsString } from 'class-validator';
+import { IsNotEmpty, IsIn, IsString, MinLength, MaxLength, Matches } from 'class-validator';
 
 import { userRoles } from '../schemas/user.schema';
 
@@ -8,6 +8,11 @@ export class CreateUserDto implements UserType {
   @IsNotEmpty()
   username: string;
 
+  @Matches(/[a-z]/, { message: 'password must contain at least one lower case letter' })
+  @Matches(/[A-Z]/, { message: 'password must contain at least one upper case letter' })
+  @Matches(/\d/, { message: 'password must contain at least one number' })
+  @MaxLength(32)
+  @MinLength(8)
   @IsString()
   @IsNotEmpty()
   password: string;
