1e3ee9220625c4a884547d53123b8e0037fcb25b
joshunrau <contact@joshuaunrau.com>
2022-12-20 14:04:09 -0600

    add tailwind

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.js';
+import './styles.css';
 
 const root = document.getElementById('root') as HTMLElement;
 ReactDOM.createRoot(root).render(
