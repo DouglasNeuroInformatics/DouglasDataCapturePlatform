e4a6d0a1d714f2f699d9efe8bd4612abde018b50
joshunrau <contact@joshuaunrau.com>
2023-01-03 11:55:40 -0500

    allow explicit any

--- packages/eslint-config/index.cjs
+++ packages/eslint-config/index.cjs
@@ -23,6 +23,7 @@ module.exports = {
       }
     ],
     '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-misused-promises': [
       'error',
       {
